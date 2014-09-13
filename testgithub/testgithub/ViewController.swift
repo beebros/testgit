34d11e14fd50806618bb7116c6f970ab3571b144
beebros <bbros@wertco.biz>
2014-09-13 21:59:58 +0200

    Test
    Modifiche di test

---
+++ testgithub/testgithub/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  testgithub
+//
+//  Created by Fabrizio Bacchin on 13/09/14.
+//  Copyright (c) 2014 wwCONSULTANTnet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var prova = 10
+        prova += 3
+        println(prova)
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
